6ddb70a532b612be604da08e42f649a1c722fce3
Michaelxk <linares.michael@gmail.com>
2018-08-21 11:36:39 -0400

    learning about vue-router

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <section>
+    <h1>Routes with Vue Router</h1>
+     <router-link to="/">Home</router-link>
+     <router-link to="/contact">Contact</router-link>
+    <hr>
+    <router-view></router-view>
+  </section>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="scss">
+</style>
